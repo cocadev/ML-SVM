54035b54e1b38be2f4a1f0a8a530fd9e84013c7c
Cocadev <40970351+cocadev@users.noreply.github.com>
2023-03-30 04:01:49 +0800

    Initial commit

---
+++ READ.ME.py
@@ -0,0 +1,5 @@
+conda create -c conda-forge -n spyder-env spyder numpy scipy pandas matplotlib sympy cython
+conda create -c conda-forge -n spyder-env spyder
+conda activate spyder-env
+conda install scikit-learn
+conda install matplotlib
